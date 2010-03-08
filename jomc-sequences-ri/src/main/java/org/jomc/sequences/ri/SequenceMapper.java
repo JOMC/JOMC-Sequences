64d7be3876c1af901e739afdf4a3b4349f20a5db
Christian Schulte <cs@schulte.it>
2010-03-08 03:37:08 +0000

    [maven-scm-plugin] Build 2010-03-08T03:36:24+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-sequences/trunk@1620 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-sequences-ri/src/main/java/org/jomc/sequences/ri/SequenceMapper.java
+++ jomc-sequences-ri/src/main/java/org/jomc/sequences/ri/SequenceMapper.java
@@ -63,7 +63,7 @@ import org.jomc.sequences.model.SequenceType;
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
 @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor",
-                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-17/jomc-tools" )
+                             comments = "See http://jomc.sourceforge.net/jomc/1.0-alpha-18-SNAPSHOT/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public interface SequenceMapper
