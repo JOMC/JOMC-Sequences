4e6445d6d629f2123da646a5c6372b3039921626
Christian Schulte <cs@schulte.it>
2010-04-27 12:46:05 +0000

    [maven-scm-plugin] Build 2010-04-27T14:44:44+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-sequences/trunk@1831 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-sequences-api/src/main/java/org/jomc/sequences/SequenceChangeListener.java
+++ jomc-sequences-api/src/main/java/org/jomc/sequences/SequenceChangeListener.java
@@ -68,7 +68,7 @@ import java.util.EventListener;
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
 @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor",
-                             comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-3-SNAPSHOT/jomc-tools" )
+                             comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-3/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public interface SequenceChangeListener extends EventListener
