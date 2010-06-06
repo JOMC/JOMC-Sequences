38f1de806279e6ef0effd70a84f624150b279709
Christian Schulte <cs@schulte.it>
2010-06-06 01:09:08 +0000

    [maven-scm-plugin] Build 2010-06-06T03:08:35+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-sequences/trunk@2008 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-sequences-api/src/main/java/org/jomc/sequences/Sequence.java
+++ jomc-sequences-api/src/main/java/org/jomc/sequences/Sequence.java
@@ -51,7 +51,7 @@ import java.util.Date;
 // SECTION-END
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
-@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-4-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
+@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-4", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public class Sequence implements Comparable<Sequence>, Cloneable, Serializable
@@ -359,7 +359,7 @@ public class Sequence implements Comparable<Sequence>, Cloneable, Serializable
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
 
     /** Creates a new {@code Sequence} instance. */
-    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-4-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
+    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-4", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4/jomc-tools" )
     public Sequence()
     {
         // SECTION-START[Default Constructor]
