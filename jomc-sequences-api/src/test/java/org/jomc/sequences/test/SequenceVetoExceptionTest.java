633d565e46202bd1da8337395fe1782eb17e0253
Christian Schulte <cs@schulte.it>
2010-06-06 01:09:31 +0000

    [maven-scm-plugin] Build 2010-06-06T03:08:35+0200
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-sequences/trunk@2009 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-sequences-api/src/test/java/org/jomc/sequences/test/SequenceVetoExceptionTest.java
+++ jomc-sequences-api/src/test/java/org/jomc/sequences/test/SequenceVetoExceptionTest.java
@@ -58,7 +58,7 @@ import static org.junit.Assert.assertTrue;
 // SECTION-END
 // SECTION-START[Annotations]
 // <editor-fold defaultstate="collapsed" desc=" Generated Annotations ">
-@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-4-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
+@javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-4", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4/jomc-tools" )
 // </editor-fold>
 // SECTION-END
 public class SequenceVetoExceptionTest
@@ -121,7 +121,7 @@ public class SequenceVetoExceptionTest
     // <editor-fold defaultstate="collapsed" desc=" Generated Constructors ">
 
     /** Creates a new {@code SequenceVetoExceptionTest} instance. */
-    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-4-SNAPSHOT", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4-SNAPSHOT/jomc-tools" )
+    @javax.annotation.Generated( value = "org.jomc.tools.SourceFileProcessor 1.0-beta-4", comments = "See http://jomc.sourceforge.net/jomc/1.0-beta-4/jomc-tools" )
     public SequenceVetoExceptionTest()
     {
         // SECTION-START[Default Constructor]
