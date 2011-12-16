6a5fc594f95391af5cab5628311ca3984ef4a2ee
Christian Schulte <cs@schulte.it>
2011-12-16 11:03:06 +0000

    [maven-scm-plugin] Build 2011-12-16T11:02:49+0000
    git-svn-id: http://svn.jomc.org/svnroot/jomc/jomc-sequences/trunk@4089 aee2edba-5628-49ba-b8b8-0e9d4ac63d57

--- jomc-sequences-utilities/src/main/java/org/jomc/sequences/util/SequencesTableModel.java
+++ jomc-sequences-utilities/src/main/java/org/jomc/sequences/util/SequencesTableModel.java
@@ -53,17 +53,10 @@ import org.jomc.sequences.SequencesSystemException;
 /**
  * Swing TableModel Java Bean for displaying and editing a system's SequenceDirectory.
  *
- * <p>
- *   This implementation is identified by {@code <org.jomc.sequences.util.SequencesTableModel>}.
- * </p>
- * <p>
- *   It provides objects named {@code <JOMC Sequences Utilities>} of the following specifications:
- *
- *   <ul>
- *     <li>{@code <javax.swing.table.TableModel>} at any specification level.</li>
- *   </ul>
- * </p>
  * <dl>
+ *   <dt><b>Identifier:</b></dt><dd>org.jomc.sequences.util.SequencesTableModel</dd>
+ *   <dt><b>Name:</b></dt><dd>JOMC Sequences Utilities</dd>
+ *   <dt><b>Implements:</b></dt><dd>javax.swing.table.TableModel</dd></dt>
  *   <dt><b>Abstract:</b></dt><dd>No</dd>
  *   <dt><b>Final:</b></dt><dd>No</dd>
  *   <dt><b>Stateless:</b></dt><dd>No</dd>
